5e6976e5c506660608b2d86f1ea113bb43c4a313
Vladyslav <vladmelhnik@gmail.com>
2023-09-20 09:17:53 +0300

    add basic fields in Game class

---
+++ Game_Account_Labwork/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Account_Labwork.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int GameRating { get; set; }
+        public string? FirstGamer { get; set; }
+        public string? SecondGamer { get; set; }
+    }
+}
